e1b9cee125bec943e85710febdffb94464f3c737
Jahnae Reese <Jahnae.reese-stige@sjsu.edu>
2022-03-13 16:31:22 -0700

    Set up LibGDX project

---
+++ android/src/com/hackslash/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.hackslash.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.hackslash.game.HackSlash;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new HackSlash(), config);
+	}
+}
