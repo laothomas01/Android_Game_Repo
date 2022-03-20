6112cc50ed3ce8bf0c89dc60c831619cab7eacaf
ngo500 <72844093+ngo500@users.noreply.github.com>
2022-03-19 19:33:35 -0700

    Update HackSlash.java
    changed default background to be less vibrant

--- core/src/com/hackslash/game/HackSlash.java
+++ core/src/com/hackslash/game/HackSlash.java
@@ -17,7 +17,7 @@ public class HackSlash extends ApplicationAdapter {
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
+		ScreenUtils.clear(0, 0.5f, 0.5f, 1);
 		batch.begin();
 		batch.draw(img, 0, 0);
 		batch.end();
