3fb076189a1e3dbdc615588985505f0a15334ea0
ngo500 <72844093+ngo500@users.noreply.github.com>
2022-04-14 20:23:21 -0700

    added basic PlayerHealthBar file

---
+++ core/src/com/hackslash/game/model/PlayerHealthBar.java
@@ -0,0 +1,27 @@
+package com.hackslash.game.model;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+
+public class PlayerHealthBar {
+
+    Player player;
+    Color cl;
+
+    public PlayerHealthBar(Player player){
+        this.player = player;
+        cl = Color.GREEN;
+    }
+
+    public void draw(ShapeRenderer sr, int size) {
+        sr.setColor(cl);
+        sr.begin(ShapeRenderer.ShapeType.Filled);
+        sr.rect(0, 0, size, 50);
+        sr.end();
+    }
+
+    public void changeColor(Color newCL){
+        this.cl = newCL;
+    }
+
+}
