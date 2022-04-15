a6f8ce433cdbdbb8665fe71af9d96331a5a06fed
ngo500 <72844093+ngo500@users.noreply.github.com>
2022-04-14 20:22:34 -0700

    added basic healthcare reference

--- core/src/com/hackslash/game/model/Player.java
+++ core/src/com/hackslash/game/model/Player.java
@@ -13,96 +13,70 @@ import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Polygon;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.FloatArray;
+import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.ShortArray;
 import com.hackslash.game.driver.HackAndSlash;
 
 public class Player extends GameObject {
-
-    float maxSpeed;
-
-
-    Vector2 center;
-    FloatArray vertices;
-
-    /**
-     * Filling in the polygon
-     */
-    Texture texture;
-    PolygonSprite polySprite;
-    PolygonSpriteBatch polyBatch;
+    Vector2 player_position;
+    int playerHealth;
 
     public Player() {
         x = Gdx.graphics.getWidth() / 2;
         y = Gdx.graphics.getHeight() / 2;
+        size = 25;
+        /**
+         * useful for vector functions
+         */
+        player_position = new Vector2(x, y);
 
+        /**
+         * with delta time, we move at speed * Number of units per second even if the frame rate drops.
+         */
+        speed = 300;
 
-        maxSpeed = 300;
-
+        playerHealth = 100;
 
     }
-
-    public void update(float dt) {
-
-
-        //set shape
-        setShape();
-
-
-    }
-
 
     public void draw(ShapeRenderer sr) {
         sr.setColor(1, 1, 1, 1);
-        sr.begin(ShapeRenderer.ShapeType.Line);
-        sr.setColor(0, 1, 0, 1);
-        sr.polygon(vertices.toArray());
-        setShape();
-
-
-        /**
-         * not sure how this code works. still trying to implement it but this loop draws the shape of our player
-         */
-//        for (int i = 0, j = shape_x.length - 1; i < shape_x.length;
-//             j = i++) {
-//            sr.line(shape_x[i], shape_y[i], shape_x[j], shape_y[j]);
-//        }
-
+        sr.begin(ShapeRenderer.ShapeType.Filled);
+        sr.rect(player_position.x, player_position.y, 20, 20);
         sr.end();
     }
 
-    void setShape() {
-
-
-        center = new Vector2(x, y);
-        vertices = new FloatArray(new float[]{center.x, center.y + 50, center.x + 50, center.y, center.x, center.y - 50, center.x - 50, center.y});
-
-
-
-        /**
-         * ----------------------------------------------------------------------------
-         */
-
-        /**
-         * all this code just to fill in a polygon. F..M...L... :( !
-         */
-        polyBatch = new PolygonSpriteBatch();
-        Pixmap pix = new Pixmap(1, 1, Pixmap.Format.RGBA8888);
-        pix.setColor(1, 1, 1, 1);
-        pix.fill();
-        texture = new Texture(pix);
-        TextureRegion textureRegion = new TextureRegion(texture);
-        EarClippingTriangulator triangulator = new EarClippingTriangulator();
-        ShortArray triangleIndices = triangulator.computeTriangles(vertices);
-        PolygonRegion polyReg = new PolygonRegion(textureRegion, vertices.toArray(), triangleIndices.toArray());
-        polySprite = new PolygonSprite(polyReg);
-        polyBatch.begin();
-        polySprite.draw(polyBatch);
-        polyBatch.end();
-
-
-        /**
-         * ----------------------------------------------------------------------------
-         */
+    public float getXPosition() {
+        return player_position.x;
     }
+
+    public float getYPosition() {
+        return player_position.y;
+    }
+
+    public float getPlayerSpeed() {
+        return speed;
+    }
+
+    public Vector2 getPlayerPosition() {
+        return player_position;
+    }
+
+    public void setSpeed(float speed) {
+        this.speed = speed;
+    }
+
+    public void setXPosition(float dx) {
+        player_position.x = dx;
+    }
+
+    public void setYPosition(float dy) {
+        player_position.y = dy;
+    }
+
+    public int getPlayerHealth(){
+        return playerHealth;
+    }
+
 
 }
