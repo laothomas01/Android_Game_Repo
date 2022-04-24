7fa3935767b8fa66854ec215aa160b8d909f7ce4
ngo500 <72844093+ngo500@users.noreply.github.com>
2022-04-23 22:55:16 -0700

    draw with sprite instead
    added more logic and dispose

--- core/src/com/hackslash/game/model/PlayerHealthBar.java
+++ core/src/com/hackslash/game/model/PlayerHealthBar.java
@@ -1,27 +1,63 @@
 package com.hackslash.game.model;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 
 public class PlayerHealthBar {
 
-    Player player;
-    Color cl;
+    Player player;  //connected player
+    Color cl;       //color of health bar
+    int currentHealth;  //current health points
+    int maxHealth = 500;
+    Sprite sprite;
+    Texture tex;
 
     public PlayerHealthBar(Player player){
         this.player = player;
         cl = Color.GREEN;
+        currentHealth = maxHealth;
+
+        tex = new Texture(Gdx.files.internal("square.png"));
+        sprite = new Sprite(tex, 0, 0, maxHealth, 50);
     }
 
-    public void draw(ShapeRenderer sr, int size) {
-        sr.setColor(cl);
-        sr.begin(ShapeRenderer.ShapeType.Filled);
-        sr.rect(250, 100, size, 50);
-        sr.end();
+    public void draw(Batch batch) {
+        batch.begin();
+        batch.setColor(cl);
+        sprite.setScale(maxHealth, 50);
+        batch.draw(tex, 250,100, currentHealth, 50);
+        batch.end();
     }
 
     public void changeColor(Color newCL){
         this.cl = newCL;
     }
+    
+    public int getCurrentHealth()
+    {
+        currentHealth = player.getPlayerHealth();
+        return currentHealth;
+    }
+
+    public void setCurrentHealth(int newHealth)
+    {
+        if(newHealth < maxHealth)
+        {
+            this.currentHealth = newHealth;
+        }
+        else
+        {
+            this.currentHealth = maxHealth;
+        }
+
+    }
+
+    public void dispose(){
+        tex.dispose();
+    }
+
 
 }
