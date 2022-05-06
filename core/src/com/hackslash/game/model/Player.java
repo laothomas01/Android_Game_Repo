70940e52ae0200f0399220f6b7f53d49ca088377
ngo500 <72844093+ngo500@users.noreply.github.com>
2022-05-05 18:41:59 -0700

    updated shootBullet function
    bullets hit enemies and disappear- targeted enemy disappears when health is gone

--- core/src/com/hackslash/game/model/Player.java
+++ core/src/com/hackslash/game/model/Player.java
@@ -6,14 +6,20 @@ import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.math.Vector2;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Queue;
+
 public class Player extends GameObject {
 
     Vector2 player_position;
     float playerHealth;
     Sprite sprite;
     Texture tex;
-
-    Actor actor;
+    float shoot_timer = 0;
+    float shoot_cooldown = 5;
+    float enemy_detection_radius = 300f;
+    float circleCenterRadius;
 
     public Player() {
         x = Gdx.graphics.getWidth() / 2;
@@ -24,8 +30,6 @@ public class Player extends GameObject {
          * useful for vector functions
          */
         player_position = new Vector2(x, y);
-
-
         /**
          * with delta time, we move at speed * Number of units per second even if the frame rate drops.
          */
@@ -36,10 +40,10 @@ public class Player extends GameObject {
 
         tex = new Texture(Gdx.files.internal("square.png"));
         sprite = new Sprite(tex, 0, 0, 20, 20);
-
+        circleCenterRadius = 100;
 
     }
-    
+
     public Player(float posX, float posY, int player_size, float player_speed) {
         x = posX;
         y = posY;
@@ -47,17 +51,17 @@ public class Player extends GameObject {
         speed = player_speed;
     }
 
-   public void draw(Batch batch) {
+    public void draw(Batch batch) {
         batch.begin();
         sprite.setPosition(player_position.x, player_position.y);
         sprite.draw(batch);
         batch.end();
+
     }
 
     public float getXPosition() {
         return player_position.x;
     }
-
 
     public float getYPosition() {
         return player_position.y;
@@ -83,17 +87,58 @@ public class Player extends GameObject {
         player_position.y = dy;
     }
 
-    public float getPlayerHealth(){
+    public float getPlayerHealth() {
         return playerHealth;
 
+    }
 
-    public Sprite getSprite()
-    {
+    public Sprite getSprite() {
         return sprite;
     }
-        
-    public void dispose(){
+
+    public void dispose() {
         tex.dispose();
+    }
+
+    public void shootBullets(ArrayList<Enemy> enemies, float dt, ArrayList<Bullet> bullets, Batch batch, Queue<Enemy> enemyQueue) {
+        shoot_timer += dt;
+        ArrayList<Enemy> removeEnemies = new ArrayList<>();
+        for (Iterator<Enemy> iterator = enemies.iterator(); iterator.hasNext();) {
+            Enemy i = iterator.next();
+            if (detectEnemy(i)) {
+                enemyQueue.add(i);
+                Enemy spotted = enemyQueue.peek();
+                if (spotted.getHealth() <= 0) {
+                    removeEnemies.add(spotted);
+                    enemyQueue.remove(spotted);
+                    spotted = enemyQueue.peek();
+                } else if (shoot_timer >= shoot_cooldown) {
+                    bullets.add(new Bullet(this.getXPosition(), this.getYPosition()));
+                    shoot_timer = 0;
+                } else {
+                    shoot_timer += dt;
+                }
+                for (Iterator<Bullet> iter = bullets.iterator(); iter.hasNext();) {
+                    Bullet x = iter.next();
+                    x.draw(batch);
+                    x.update(dt, spotted);
+                    if (x.hasHit()) {
+                        iter.remove();
+                        spotted.subtractHealth();
+                        System.out.println("BULLET:" + i + "HAS HIT!");
+                    }
+                }
+            }
+            else{}
+        }
+        enemies.removeAll(removeEnemies);
+    }
+
+    public boolean detectEnemy(Enemy e) {
+        if (Vector2.dst(this.getXPosition(), this.getYPosition(), e.getXPosition(), e.getYPosition()) <= enemy_detection_radius) {
+            return true;
+        }
+        return false;
     }
 
 
