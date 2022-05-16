6aca41a1f188d4f8c220b1f489bda7f94ca7f25f
ngo500 <72844093+ngo500@users.noreply.github.com>
2022-05-15 18:56:19 -0700

    brought up to date

--- core/src/com/hackslash/game/model/Player.java
+++ core/src/com/hackslash/game/model/Player.java
@@ -4,44 +4,34 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
-
 import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.Queue;
 
 public class Player extends GameObject {
 
-    Vector2 player_position;
+    float MAX_BULLETS = 1;
     float playerHealth;
     Sprite sprite;
     Texture tex;
-    float shoot_timer = 0;
-    float shoot_cooldown = 5;
-    float enemy_detection_radius = 300f;
-    float circleCenterRadius;
+    float fireTimer;
+    float fireTime;
+    boolean remove;
+    float enemy_detection_radius = 500f;
+    ArrayList<Bullet> bullets;
 
     public Player() {
         x = Gdx.graphics.getWidth() / 2;
         y = Gdx.graphics.getHeight() / 2;
         size = 25;
-
-        /**
-         * useful for vector functions
-         */
-        player_position = new Vector2(x, y);
-        /**
-         * with delta time, we move at speed * Number of units per second even if the frame rate drops.
-         */
         speed = 300f;
-
-        //player health for health bar
-        playerHealth = 500f;
+        playerHealth = 1000f;
 
         tex = new Texture(Gdx.files.internal("square.png"));
         sprite = new Sprite(tex, 0, 0, 20, 20);
-        circleCenterRadius = 100;
-
+        bullets = new ArrayList<Bullet>();
+        fireTimer = 0;
+        fireTime = 2f;
     }
 
     public Player(float posX, float posY, int player_size, float player_speed) {
@@ -51,28 +41,59 @@ public class Player extends GameObject {
         speed = player_speed;
     }
 
+    public void update(float dt, Batch batch, Enemy e) {
+        if (detectEnemy(e)) {
+            if (!e.isHit()) {
+                if (fireTimer > fireTime) {
+                    fireTimer = 0;
+                    radians = MathUtils.atan2(e.getYPosition() - y, e.getXPosition() - x);
+                    loadBullets();
+                } else {
+                    fireTimer += dt;
+                    fireBullets(dt, batch);
+                }
+            }
+        }
+    }
+
+    public void loadBullets() {
+        if (bullets.size() == MAX_BULLETS) {
+            return;
+        }
+        bullets.add(new Bullet(x, y, radians));
+    }
+
+    public void fireBullets(float dt, Batch batch) {
+        if (!bullets.isEmpty()) {
+            bullets.get(0).draw(batch);
+            bullets.get(0).update(dt);
+            if (bullets.get(0).shouldRemove()) {
+                bullets.remove(0);
+            }
+        }
+    }
+
     public void draw(Batch batch) {
         batch.begin();
-        sprite.setPosition(player_position.x, player_position.y);
+        sprite.setPosition(x, y);
         sprite.draw(batch);
         batch.end();
+    }
 
+    public ArrayList<Bullet> getBullets() {
+        return bullets;
     }
 
     public float getXPosition() {
-        return player_position.x;
+        return x;
     }
 
     public float getYPosition() {
-        return player_position.y;
+        return y;
     }
 
     public float getPlayerSpeed() {
         return speed;
-    }
-
-    public Vector2 getPlayerPosition() {
-        return player_position;
     }
 
     public void setSpeed(float speed) {
@@ -80,65 +101,40 @@ public class Player extends GameObject {
     }
 
     public void setXPosition(float dx) {
-        player_position.x = dx;
+        x = dx;
     }
 
     public void setYPosition(float dy) {
-        player_position.y = dy;
+        y = dy;
     }
 
     public float getPlayerHealth() {
         return playerHealth;
-
     }
 
     public Sprite getSprite() {
         return sprite;
     }
 
-    public void dispose() {
-        tex.dispose();
-    }
-
-    public void shootBullets(ArrayList<Enemy> enemies, float dt, ArrayList<Bullet> bullets, Batch batch, Queue<Enemy> enemyQueue) {
-        shoot_timer += dt;
-        ArrayList<Enemy> removeEnemies = new ArrayList<>();
-        for (Iterator<Enemy> iterator = enemies.iterator(); iterator.hasNext();) {
-            Enemy i = iterator.next();
-            if (detectEnemy(i)) {
-                enemyQueue.add(i);
-                Enemy spotted = enemyQueue.peek();
-                if (spotted.getHealth() <= 0) {
-                    removeEnemies.add(spotted);
-                    enemyQueue.remove(spotted);
-                    spotted = enemyQueue.peek();
-                } else if (shoot_timer >= shoot_cooldown) {
-                    bullets.add(new Bullet(this.getXPosition(), this.getYPosition()));
-                    shoot_timer = 0;
-                } else {
-                    shoot_timer += dt;
-                }
-                for (Iterator<Bullet> iter = bullets.iterator(); iter.hasNext();) {
-                    Bullet x = iter.next();
-                    x.draw(batch);
-                    x.update(dt, spotted);
-                    if (x.hasHit()) {
-                        iter.remove();
-                        spotted.subtractHealth();
-                        System.out.println("BULLET:" + i + "HAS HIT!");
-                    }
-                }
-            }
-            else{}
-        }
-        enemies.removeAll(removeEnemies);
-    }
-
     public boolean detectEnemy(Enemy e) {
-        if (Vector2.dst(this.getXPosition(), this.getYPosition(), e.getXPosition(), e.getYPosition()) <= enemy_detection_radius) {
+        if (Vector2.dst(this.getXPosition(), this.getYPosition(), e.getXPosition(), e.getYPosition()) < enemy_detection_radius) {
             return true;
         }
         return false;
+
+    }
+
+    public boolean shouldRemove() {
+        return remove;
+    }
+
+    public void takeDamage(float h) {
+        playerHealth -= h;
+    }
+
+    public void dispose()
+    {
+        tex.dispose();
     }
 
 
