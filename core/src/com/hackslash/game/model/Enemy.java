60baa83520abac588ec75ebb667e2807cf15ce51
ngo500 <72844093+ngo500@users.noreply.github.com>
2022-05-15 18:55:35 -0700

    brought up to date

--- core/src/com/hackslash/game/model/Enemy.java
+++ core/src/com/hackslash/game/model/Enemy.java
@@ -9,92 +9,109 @@ import com.badlogic.gdx.math.Vector2;
 
 public class Enemy extends GameObject {
 
-    int radius;
-
-    Vector2 position;
     Sprite sprite;
     Texture tex;
+    boolean remove;
+    static boolean hit;
 
-    public Enemy() {
-        x = Gdx.graphics.getWidth() / 4;
-        y = Gdx.graphics.getHeight() / 4;
-        position = new Vector2(x, y);
-        enemy_direction = new Vector2();
-        radius = 20;
-        size = radius;
-        speed = 100;
-        damage = 1;
-        health = 1;
-    }
-    
-    public Enemy(float posX, float posY, float enemy_speed, float enemy_damage, float enemy_size, float enemy_health) {
+    public Enemy(float posX, float posY, float enemy_speed, float enemy_damage, int enemy_size, float enemy_health) {
         x = posX;
         y = posY;
-        position = new Vector2(x, y);
         speed = enemy_speed;
         damage = enemy_damage;
         size = enemy_size;
         health = enemy_health;
-
         tex = new Texture(Gdx.files.internal("circle.png"));
         sprite = new Sprite(tex, 0, 0, 20, 20);
+        remove = false;
+        hit = false;
     }
 
     public void update(float dt, Player player) {
         basic_enemy_AI(player, dt);
+
+        if (this.getHealth() <= 0) {
+            remove = true;
+        }
     }
 
     public void draw(Batch batch) {
         batch.begin();
         batch.setColor(Color.WHITE);
-        sprite.setScale(getEnemySize(), getEnemySize());
-        sprite.setPosition(getXPosition(),getYPosition());
-        atch.draw(tex, getXPosition(),getYPosition(), (getEnemySize()*2), (getEnemySize()*2));
+        sprite.setScale(getSize(), getSize());
+        sprite.setPosition(getXPosition(), getYPosition());
+        batch.draw(tex, getXPosition(), getYPosition(), (getSize() * 2), (getSize() * 2));
         batch.end();
     }
 
     public void basic_enemy_AI(Player player, float dt) {
-
-        /**
-         * two different ways to have the enemy or any object follow another object in the most basic form
-         */
-        Vector2 playerPos = player.getPlayerPosition();
         Vector2 enemy_direction = new Vector2();
-        enemy_direction.x = (playerPos.x + 20) - (getEnemyPosition().x + 20);
-        enemy_direction.y = (playerPos.y + 20) - (getEnemyPosition().y + 20);
+        dx = (player.getXPosition() + 20) - (this.getXPosition() + 20);
+        dy = (player.getYPosition() + 20) - (this.getYPosition() + 20);
+
+        enemy_direction.x = dx;
+        enemy_direction.y = dy;
+
         enemy_direction.nor();
-        getEnemyPosition().x += enemy_direction.x * speed * dt;
-        getEnemyPosition().y += enemy_direction.y * speed * dt;
+
+        x += enemy_direction.x * speed * dt;
+        y += enemy_direction.y * speed * dt;
+    }
+
+    boolean isHit() {
+        return hit;
     }
 
     public float getXPosition() {
-        return position.x;
+        return x;
     }
 
     public float getYPosition() {
-        return position.y;
+        return y;
     }
 
-    public Vector2 getEnemyPosition() {
-        return position;
-    }
-    
-    public void setEnemySize(float size) {
-        this.size = size;
+    public void setSize(int s) {
+        size += s;
     }
 
-    public float getEnemySize() {
+    public int getSize() {
         return size;
     }
 
-    public Sprite getSprite()
-    {
+    public Sprite getSprite() {
         return sprite;
     }
-    
-    public Texture getTex() 
-    { 
-        return tex; 
+
+    public Texture getTex() {
+        return tex;
+    }
+
+    public float getHealth() {
+        return health;
+    }
+
+    public void takeDamage(float damage) {
+        this.health -= damage;
+    }
+
+    public boolean shouldRemove() {
+        return remove;
+    }
+
+    public void setRemove(boolean r) {
+        remove = r;
+    }
+
+    public void setSpeed(float s) {
+        speed += s;
+    }
+
+    public float getSpeed() {
+        return speed;
+    }
+
+    public float getRadians() {
+        return radians;
     }
 
 }
