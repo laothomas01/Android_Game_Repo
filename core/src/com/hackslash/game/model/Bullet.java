fb2c6ba9ccb1c2e72877fec300cc95094003afec
ngo500 <72844093+ngo500@users.noreply.github.com>
2022-05-15 18:54:43 -0700

    brought up to date

--- core/src/com/hackslash/game/model/Bullet.java
+++ core/src/com/hackslash/game/model/Bullet.java
@@ -5,25 +5,36 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
 
 public class Bullet extends GameObject {
-
-    Vector2 position;
     Sprite sprite;
     Texture tex;
-    int radius;
     boolean hit;
+    float lifeSpan;
+    float maxLife;
+    boolean remove;
+    int radius;
 
-    public Bullet(float x, float y) {
+    public Bullet(float x, float y, float radians) {
         this.x = x;
         this.y = y;
-        position = new Vector2(x, y);
-        speed = 1f;
+        this.radians = radians;
+        speed = 300f;
+        size = 15;
         radius = 15;
         tex = new Texture(Gdx.files.internal("circle.png"));
-        sprite = new Sprite(tex, 0, 0, radius, radius);
+        sprite = new Sprite(tex, 0, 0, size, size);
+        lifeSpan = 0;
+        maxLife = 5;
+        damage = 1;
+        remove = false;
         hit = false;
+        //  x direction
+        dx = MathUtils.cos(radians) * speed;
+        //y direction
+        dy = MathUtils.sin(radians) * speed;
     }
 
     public void draw(Batch batch) {
@@ -37,20 +48,13 @@ public class Bullet extends GameObject {
     /**
      * Bullet will target 1 enemy at a time
      */
-    public void update(float dt, Enemy enemy) {
-        Vector2 enemy_pos = enemy.getEnemyPosition();
-        Vector2 bullet_direction = new Vector2();
+    public void update(float dt) {
+        x += dx * dt;
+        y += dy * dt;
 
-        bullet_direction.x = (enemy_pos.x + 20) - (this.getXPosition() + 20);
-        bullet_direction.y = (enemy_pos.y + 20) - (this.getYPosition() + 20);
-
-        getBulletPosition().x += bullet_direction.x * getBulletSpeed() * dt;
-        getBulletPosition().y += bullet_direction.y * getBulletSpeed() * dt;
-
-        float distance = Vector2.dst(enemy_pos.x, enemy_pos.y, getBulletPosition().x, getBulletPosition().y);
-        float sumRadius = this.getRadius() + enemy.getEnemySize();
-        if (distance < sumRadius) {
-            hit = true;
+        lifeSpan += dt;
+        if (lifeSpan > maxLife) {
+            remove = true;
         }
     }
 
@@ -62,8 +66,21 @@ public class Bullet extends GameObject {
         return radius;
     }
 
-    public Vector2 getBulletPosition() {
-        return position;
+    public boolean shouldRemove() {
+        return remove;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public float getDamage() {
+        return damage;
+    }
+
+    public void set_pos(float x, float y) {
+        this.x = x;
+        this.y = y;
     }
 
     public Sprite getSprite() {
@@ -71,15 +88,25 @@ public class Bullet extends GameObject {
     }
 
     public float getXPosition() {
-        return position.x;
+        return x;
     }
 
     public float getYPosition() {
-        return position.y;
+        return y;
     }
 
     public float getBulletSpeed() {
         return speed;
+    }
+
+    public boolean intersect(Enemy e) {
+        float total_radius = size + e.getSize();
+        float distance = Vector2.dst(e.getXPosition(), e.getYPosition(), x, y);
+        if (distance < total_radius) {
+            remove = true;
+            return true;
+        }
+        return false;
     }
 
 }
